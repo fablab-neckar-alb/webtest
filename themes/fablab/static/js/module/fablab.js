2831eb56bf5c9b01fac52f2d3d98ad0bd9903f5e
Joris Karl Dzaack <hola@arebours.de>
2019-11-28 17:46:01 +0100

    Generated javascript and css files.

--- themes/fablab/static/js/module/fablab.js
+++ themes/fablab/static/js/module/fablab.js
@@ -1,5 +1,5 @@
 /*
  domready (c) Dustin Diaz 2014 - License MIT
 */
-var b,f=void 0;b=(f={exports:{}},function(g){!function(d,a){g.exports=a()}('domready',function(){var d=[],a,c=document,e=(c.documentElement.doScroll?/^loaded|^c/:/^loaded|^i|^c/).test(c.readyState);e||c.addEventListener('DOMContentLoaded',a=function(){c.removeEventListener('DOMContentLoaded',a);for(e=1;a=d.shift();)a()});return function(a){e?setTimeout(a,0):d.push(a)}})}(f,f.exports),f.exports);let h=window.console.debug.bind(window.console,'DEBUG:');
-b(()=>{h('test');document.documentElement.className=document.documentElement.className.replace(/\bno-js\b/,'js')});//# sourceMappingURL=fablab.js.map
+var e=void 0;let g=(b,f,a)=>new Promise((d,c)=>{b instanceof window.HTMLElement?(c=b.className.replace(new RegExp(`\\b${f}\\b`),a),b.className=c,d(c)):c(Error('Object not a HTMLELement'))});
+(e={exports:{}},function(b){!function(f,a){b.exports=a()}('domready',function(){var b=[],a,d=document,c=(d.documentElement.doScroll?/^loaded|^c/:/^loaded|^i|^c/).test(d.readyState);c||d.addEventListener('DOMContentLoaded',a=function(){d.removeEventListener('DOMContentLoaded',a);for(c=1;a=b.shift();)a()});return function(a){c?setTimeout(a,0):b.push(a)}})}(e,e.exports),e.exports)(()=>{g(window.document.documentElement,'no-js','js')});//# sourceMappingURL=fablab.js.map
--- themes/fablab/static/js/nomodule/fablab.js
+++ themes/fablab/static/js/nomodule/fablab.js
@@ -1,6 +1,6 @@
 /*
  domready (c) Dustin Diaz 2014 - License MIT
 */
-'use strict';System.register([],function(){return{execute:function(){var e=function(c,b){return b={exports:{}},c(b,b.exports),b.exports}(function(c){!function(b,a){c.exports=a()}('domready',function(){var b=[],a,d=document,c=(d.documentElement.doScroll?/^loaded|^c/:/^loaded|^i|^c/).test(d.readyState);c||d.addEventListener('DOMContentLoaded',a=function(){d.removeEventListener('DOMContentLoaded',a);for(c=1;a=b.shift();)a()});return function(a){c?setTimeout(a,0):b.push(a)}})});let f=window.console.debug.bind(window.console,
-'DEBUG:');e(()=>{f('test');document.documentElement.className=document.documentElement.className.replace(/\bno-js\b/,'js')})}}});
+'use strict';System.register([],function(){return{execute:function(){let f=(a,b,c)=>new Promise((e,d)=>{a instanceof window.HTMLElement?(d=a.className.replace(new RegExp(`\\b${b}\\b`),c),a.className=d,e(d)):d(Error('Object not a HTMLELement'))});(function(a,b){return b={exports:{}},a(b,b.exports),b.exports})(function(a){!function(b,c){a.exports=c()}('domready',function(){var b=[],c,a=document,d=(a.documentElement.doScroll?/^loaded|^c/:/^loaded|^i|^c/).test(a.readyState);d||a.addEventListener('DOMContentLoaded',
+c=function(){a.removeEventListener('DOMContentLoaded',c);for(d=1;c=b.shift();)c()});return function(a){d?setTimeout(a,0):b.push(a)}})})(()=>{f(window.document.documentElement,'no-js','js')})}}});
 //# sourceMappingURL=fablab.js.map
